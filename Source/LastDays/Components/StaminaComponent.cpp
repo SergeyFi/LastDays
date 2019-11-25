6ea86f5d5d60261c0e02904793204b975ce96a32
Sergey Firsov <zlofks@gmail.com>
2019-11-25 20:19:22 +0300

    Add stamina component

---
+++ Source/LastDays/Components/StaminaComponent.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaminaComponent.h"
+
+// Sets default values for this component's properties
+UStaminaComponent::UStaminaComponent()
+{
+
+}
+
+
+// Called when the game starts
+void UStaminaComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
---
+++ Source/LastDays/Components/StaminaComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StaminaComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LASTDAYS_API UStaminaComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UStaminaComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+		
+};
