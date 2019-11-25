8f223a64d56de9a9e3f751a344b8551a9ae1d04c
Sergey Firsov <zlofks@gmail.com>
2019-11-25 16:05:15 +0300

    Add Human character

---
+++ Source/LastDays/Characters/Human/HumanCharacter.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter/BaseCharacter.h"
+#include "HumanCharacter.generated.h"
+
+
+UCLASS()
+class LASTDAYS_API AHumanCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
