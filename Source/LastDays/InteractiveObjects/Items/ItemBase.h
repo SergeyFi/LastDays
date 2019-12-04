e48ab3bde91ac058da4ad45f669a653f404f5e3e
Sergey Firsov <zlofks@gmail.com>
2019-12-04 14:20:23 +0300

    Add base classes

---
+++ Source/LastDays/InteractiveObjects/Items/ItemBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveObjects/BaseInteractiveObject.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class LASTDAYS_API AItemBase : public ABaseInteractiveObject
+{
+	GENERATED_BODY()
+
+public:
+
+	AItemBase();
+	
+};
