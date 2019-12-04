6887293f12fde8a96b98694a2642027056990919
Sergey Firsov <zlofks@gmail.com>
2019-12-04 13:48:19 +0300

    Add base interactive object

---
+++ Source/LastDays/InteractiveObjects/BaseInteractiveObject.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseInteractiveObject.generated.h"
+
+UCLASS()
+class LASTDAYS_API ABaseInteractiveObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseInteractiveObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void Use();
+};
